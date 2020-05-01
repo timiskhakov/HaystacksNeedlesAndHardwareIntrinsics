4097642ebc5daeb2293d68b133cd9ee70701606f
Tim Iskhakov <timiskhakov@gmail.com>
2020-05-01 21:40:50 +0300

    Add benchmarks

--- HardwareIntrinsicsAndInstructionPipelining/Comparison.cs
+++ HardwareIntrinsicsAndInstructionPipelining/Comparison.cs
@@ -1,7 +1,41 @@
+using System;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+
 namespace HardwareIntrinsicsAndInstructionPipelining
 {
     public class Comparison
     {
+        private readonly Random _random = new Random();
+        private double[] _input;
         
+        [Params(1_000, 10_000, 100_000, 1_000_000)]
+        public int N;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _input = Enumerable.Range(0, N)
+                .Select(_ => _random.NextDouble())
+                .ToArray();
+        }
+        
+        [Benchmark]
+        public byte[] BlockCopy()
+        {
+            return ArrayConverter.GetBytes(_input);
+        }
+        
+        [Benchmark]
+        public byte[] Vectors()
+        {
+            return ArrayConverter.GetBytesVectors(_input);
+        }
+        
+        [Benchmark]
+        public byte[] VectorsPipelined()
+        {
+            return ArrayConverter.GetBytesVectorsPipelined(_input);
+        }
     }
 }
